674c4ff02a7562fc4e49d946ec3456d529b0082f
zhid0399123 <zhid0399123@gmail.com>
2024-03-07 20:03:26 +0300

    feat: get classes when criterias are fulfilled
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

---
+++ src/util/conditional-classes.js
@@ -0,0 +1,13 @@
+function getConditionalClasses (criterias, styleKey, value) {
+  const aggregatedClasses = []
+  const _criterias = Object.entries(criterias)
+  const itFulfillsCriterias = _criterias.filter(
+    ([_, criteria]) => typeof criteria === 'string' && criteria === value
+  )
+  if (itFulfillsCriterias[0]) {
+    aggregatedClasses.push([crypto.randomUUID(), styleKey])
+  }
+  return aggregatedClasses.flat()
+}
+
+module.exports = getConditionalClasses
