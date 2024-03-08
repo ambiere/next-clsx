654e8a26476444f5151077485119b082923dc69c
zhid0399123 <zhid0399123@gmail.com>
2024-03-08 19:42:03 +0300

    fix: select keys from index 0 before spliting
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const getPropClasses = require('./util/prop-classes')
+const getConditionalClasses = require('./util/conditional-classes')
+
+function nextClx (style, condition = {}) {
+  return (key, ...args) => {
+    let i = 0
+    const classes = []
+    let _key = key[0].split(' ')
+
+    _key.push(args)
+    _key = _key.flat().filter((val) => val !== '')
+
+    while (i < _key.length) {
+      Object.entries(style[_key[i]]).map(([key, val]) => {
+        const conditionalClasses = getConditionalClasses(condition, key, val)
+        const propclasses = getPropClasses(condition, key, val)
+        if (conditionalClasses.length) {
+          return classes.push(conditionalClasses)
+        }
+        if (propclasses.length) {
+          return propclasses.map((c) => classes.push(c))
+        }
+        return classes.push([key, val])
+      })
+      i++
+    }
+
+    return classes
+      .filter(([_, str]) => typeof str === 'string')
+      .reduce((str, cls) => str + ` ${cls[1]}`, '')
+      .trim()
+  }
+}
+
+module.exports = nextClx
+module.exports.nextClx = nextClx
