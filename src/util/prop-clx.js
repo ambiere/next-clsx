d7267425d7e26f782cd521a4e8e3284ed9b1931d
zhid0399123 <zhid0399123@gmail.com>
2024-03-20 18:22:20 +0300

    fix: fixed styling props (#2)
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- src/util/prop-clx.js
+++ src/util/prop-clx.js
@@ -1,12 +1,12 @@
-function propClx (c, k, val) {
+function propsClx (c, k, val) {
   const aggClx = []
   const _c = Object.entries(c)
   const ok = _c.filter(([key]) => key === k)
 
   if (ok[0]) {
-    const ps = Object.values(c.prop)
+    const ps = Object.values(c.props)
     ps.map((p) => {
-      const match = val.filter((v) => c.p[v] === p)
+      const match = val.filter((v) => c.props[v] === p)
       if (match[0]) return aggClx.push([p + 'key', p])
       return -1
     })
@@ -15,4 +15,4 @@ function propClx (c, k, val) {
   return aggClx
 }
 
-module.exports = propClx
+module.exports = propsClx
