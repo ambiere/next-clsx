bb498b1c42c13cb3361e2100a411ca540054af67
zhid0399123 <zhid0399123@gmail.com>
2024-03-26 12:51:57 +0300

    chore: deleted
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- types/prop-clx.d.mts
+++
@@ -1,5 +0,0 @@
-export type Props = { [key: string]: string };
-export type Condition = { [key: string]: string | boolean | Props };
-
-export function propClx(c: Condition, k: string, val: string): string[][];
-export default propClx;
--- types/prop-clx.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace propClx {
-  type Props = { [key: string]: string };
-  type Condition = { [key: string]: string | boolean | Props };
-  function propClx(c: Condition, k: string, val: string): string[][];
-}
-
-declare function propClx(c: Condition, k: string, val: string): string[][];
-
-export = propClx;
